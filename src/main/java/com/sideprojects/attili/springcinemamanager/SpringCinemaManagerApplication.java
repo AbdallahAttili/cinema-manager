7916d24f0a5474146fcb07e58c0dc182fe352bba
Abdallah <abdallahaattili@gmail.com>
2024-07-22 00:12:17 +0300

    starting the project

---
+++ src/main/java/com/sideprojects/attili/springcinemamanager/SpringCinemaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sideprojects.attili.springcinemamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCinemaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCinemaManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideprojects/attili/springcinemamanager/SpringCinemaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideprojects.attili.springcinemamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCinemaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
